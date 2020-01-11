f8dc431b15c2ff6bcbf1d5a108c2dc1ebdee4350
Micah <micahdshackelford@gmail.com>
2020-01-11 15:29:35 -0500

    Added SSR for registration & login mockups

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static('./public/dist'));
 
+app.get('/register', (req,res) => {
+    res.sendFile("index.html", { root: "./public/dist" });
+});
+
+app.get('/login', (req,res) => {
+    res.sendFile("index.html", { root: "./public/dist" });
+});
+
 app.listen(PORT, () => {
     console.log(`[InterviewPal-Server] Online & listening on port ${PORT}`);
 });
